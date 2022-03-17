d483d1d3b805e992f8052765e4058b2db54fc8c0
Thed <theodore1seriakov@gmail.com>
2022-03-17 18:26:00 +0600

    My Calc commit

---
+++ app/src/main/java/com/example/mycalculator/StringCalculator.kt
@@ -0,0 +1,25 @@
+package com.example.mycalculator
+
+class StringCalculator {
+    fun calculate(calculationList: List<String>): Int {
+        var currentOp: String = ""
+        var currentNumber = 0
+
+        calculationList.forEach { token ->
+            when {
+                token.equals("+")
+                        || token.equals("/")
+                        || token.equals("*")
+                        || token.equals("-") -> currentOp = token
+
+                currentOp.equals("-") -> currentNumber -= token.toInt()
+                currentOp.equals("/") -> currentNumber /= token.toInt()
+                currentOp.equals("*") -> currentNumber *= token.toInt()
+                else -> currentNumber += token.toInt()
+
+            }
+        }
+
+        return currentNumber
+    }
+}
